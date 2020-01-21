8905c3614f6938f3f2bb56da21bcb18e6671c7e4
Jhonathas César <jhonathascesar232@gmail.com>
2020-01-21 16:25:59 -0300

    Corrigindo alguns erros de sintaxe

--- js/animar_menu.js
+++ js/animar_menu.js
@@ -3,10 +3,10 @@ const $sidenav = document.querySelector("#sidenav");
 const $overlay = document.querySelector(".overlay");
 
 $btn_menu_abrir.addEventListener("click", function(){
-    $sidenav.style.left=0;
-    $overlay.style.display="block";
+    $sidenav.classList.add("mostrar-sidenav");
+    $overlay.classList.add("mostrar-overlay");
 })
 $overlay.addEventListener("click", function(){
-    $sidenav.style.left="-360px";
-    $overlay.style.display="none";
+    $sidenav.classList.remove("mostrar-sidenav");
+    $overlay.classList.remove("mostrar-overlay");
 })
